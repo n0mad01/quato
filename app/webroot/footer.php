7120b4dd7a1a73d878a95ea9aa7d0cbc7c15f100
Adrian Soluch <adrian@soluch.at>
2012-04-22 14:54:04 +0200

    creds+

--- app/webroot/footer.php
+++ app/webroot/footer.php
@@ -1,7 +1,7 @@
 <div class="container footer">
         <div class="row">
                 <div class="twelvecol">
-					<p>Quato twitter framework by Adrian Soluch</p>
+					<p style="font-size:10px;float:right;">Quato twitter framework by Adrian Soluch</p>
                 </div>
         </div>
 </div>
